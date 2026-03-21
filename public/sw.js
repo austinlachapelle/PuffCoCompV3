15f65c9f812ee33de06ca6b321910dbaeae32db3
Austin Lachapelle <lachapellea@my.lanecc.edu>
2026-03-21 16:46:47 -0700

    Add files via upload

--- public/sw.js
+++ public/sw.js
@@ -1,14 +1,1 @@
-self.addEventListener("install", e => { self.skipWaiting(); });
-self.addEventListener("activate", e => { e.waitUntil(clients.claim()); });
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request).then(res => {
-      if (res.ok && e.request.method === "GET") {
-        const clone = res.clone();
-        caches.open("puffco-v3").then(c => c.put(e.request, clone));
-      }
-      return res;
-    }).catch(() => caches.match(e.request))
-    )
-  );
-});
+self.addEventListener("install",e=>{self.skipWaiting()});self.addEventListener("activate",e=>{e.waitUntil(clients.claim())});self.addEventListener("fetch",e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{if(res.ok&&e.request.method==="GET"){const c=res.clone();caches.open("puffco-v3").then(ca=>ca.put(e.request,c))}return res}).catch(()=>caches.match(e.request))))});
